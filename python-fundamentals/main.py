246148ae00972bfa45c6c6c8cc83b44e87f6dace
microprixs <arpit.mishra@microprixs.in>
2025-07-31 14:01:16 +0530

    Python fundamentals

---
+++ python-fundamentals/main.py
@@ -0,0 +1,25 @@
+# print("Namastey Youtube, I am learning Machine Learning!")
+
+name = "Sher Coder"
+# print(name[0:4])
+# print(name[5:])
+
+number = 786
+# print(type(str(number)))
+# print(bool(number))
+
+
+#  Their are some truthy and falsely values
+#  Their are total 7 falsely values
+
+#  0,False,'',0.0,[],(),{}
+# print(bool([]))
+
+
+#  Implicit Type Conversion -> Python automatically converts from 1 data type to another.
+# Explicit Type Conversion -> User manually converts from 1 data type to another using built-in functions.
+
+
+# name = input("Enter Your Name: ")
+# age = int(input("Enter your age: "))
+# print(f"Hello My name is {name} and I am {age} years old!")

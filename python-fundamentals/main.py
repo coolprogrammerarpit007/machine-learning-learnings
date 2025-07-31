86e2975f6e9ed42b80cf88a4b8ab3a7ca342625d
microprixs <arpit.mishra@microprixs.in>
2025-07-31 17:43:38 +0530

    conditional statments in python

--- python-fundamentals/main.py
+++ python-fundamentals/main.py
@@ -23,3 +23,98 @@ number = 786
 # name = input("Enter Your Name: ")
 # age = int(input("Enter your age: "))
 # print(f"Hello My name is {name} and I am {age} years old!")
+
+
+
+#  Operators 
+
+
+a = 12
+b = 20
+# addition = a + b
+# sub = b - a
+# mul = a * b
+# division = a / b
+# print(addition,sub,mul,division)
+# print(a%b)
+
+# print(75 > 23 and 85 < 140)
+# print(not 12 == 12)
+
+
+# False
+# True
+# True
+# False
+
+
+#  Conditional Statements in python
+
+#  Conditional 
+
+# num1 = int(input("Enter Number 1: "))
+# num2 = int(input("Enter Number 2: "))
+
+# if(num1 > num2):
+#     print(f"Number {num1} is greater than {num2}. ")
+
+# else:
+#     print(f"{num2} is greater than to {num1}")
+
+
+# gender = input("Enter your Gender(M/F): ").upper()
+# result = "Sir" if gender == "M" else "Madam"
+# print(f"Good Morning {result}")
+
+# number = int(input("Enter Number: "))
+# is_even = "Even" if number % 2 == 0 else "Odd"
+# print(f"Number is {is_even}")
+
+# person_name = input("Enter Name: ")
+# person_age = input("Enter Age: ")
+# age = int(person_age)
+# if age >= 18:
+#     print(f"{person_name}, you are eligible to vote!")
+# else:
+#     print(f"{person_name}, you are not eligible to vote!, their are still {18 -int( person_age)} years remaining!")
+
+
+
+# Check The Leap Year Program
+
+# year = int(input("Enter Year: "))
+
+
+# if year % 100 != 0 and year % 4 == 0:
+#     print("Leap Year")
+
+# elif year % 100 == 0 and year % 400 == 0:
+#     print("Leap Year")
+
+# else:
+#     print("Not A LEAP YEAR!")
+
+
+# take the input of temperature in celsiusX
+# @ Below 0°C → "Freezing Cold b
+# @ 0°C to 10°C → "Very Cold b
+# @ 10°C to 20°C → "Cold b
+# @ 20°C to 30°C → "Pleasant b
+# @ 30°C to 40°C → "Hot b
+# @ Above 40°C → "Very Hot "
+
+# temp = float(input("Enter Temp degree celsius: "))
+# if temp < 0:
+#     print("Freezing Cold")
+# elif temp >= 0 and temp < 10:
+#     print("Very Cold")
+# elif temp >= 10 and temp < 20:
+#     print("Cold")
+# elif temp >= 20 and temp < 30:
+#     print("Pleasant")
+# elif temp >= 30 and temp < 40:
+#     print("Hot")
+# else:
+#     print("Very Hot!")
+
+

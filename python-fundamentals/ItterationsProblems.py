79b2d1ffe0a7532fd49400efc752909971e26b01
microprixs <arpit.mishra@microprixs.in>
2025-08-01 17:25:34 +0530

    For Loop Pratice Questions done

--- python-fundamentals/ItterationsProblems.py
+++ python-fundamentals/ItterationsProblems.py
@@ -36,8 +36,8 @@
 
 # print(f"Factorial Of Natural Numbers are: {fact}")
 
-sum_of_even_numbers = 0
-sum_of_odd_numbers = 0
+# sum_of_even_numbers = 0
+# sum_of_odd_numbers = 0
 
 # num = int(input("Enter Number: "))
 
@@ -63,22 +63,118 @@ sum_of_odd_numbers = 0
 
 
 
-#  To check if a number is perfect number or not
+# #  To check if a number is perfect number or not
 
-number = int(input("Enter Number: "))
-factors = []
+# number = int(input("Enter Number: "))
+# factors = []
 
-for index in range(1,number):
-    if number % index == 0:
-        factors.append(index)
+# for index in range(1,number):
+#     if number % index == 0:
+#         factors.append(index)
 
-    else:
-        continue
+#     else:
+#         continue
 
 
-sum_of_factors = sum(factors)
-if  sum_of_factors == number:
-    print(f"Number {number} is a Perfect Number")
+# sum_of_factors = sum(factors)
+# if  sum_of_factors == number:
+#     print(f"Number {number} is a Perfect Number")
 
-else:
-    print(f"Number {number} is not a Perfect Number.")
+# else:
+#     print(f"Number {number} is not a Perfect Number.")
+
+
+
+# To check whether the given number is Prime Number or Not
+# import math
+
+# number = int(input("Enter Number: "))
+# is_prime = True
+
+# if number == 2:
+#     is_prime
+
+# if number <= 0:
+#     print("Enter a valid Positive Number")
+
+# if number > 2:
+#     for digit in range(3,number):
+#         if number % digit == 0:
+#             is_prime = False
+#             print("Prime Number Detected!")
+#             break
+
+
+#         else:
+#             continue
+
+        
+
+
+# if is_prime:
+#     print(f"Number: {number} is Prime Number")
+
+# else:
+#     print(f"Number: {number} is not a prime number!")
+            
+
+
+# Program to reverse a string
+
+# input_string = input("Enter String: ")
+# reverse_str = ''
+# for index in range(len(input_string)-1,-1,-1):
+#     reverse_str += input_string[index]
+
+
+
+# print(f"Input String: {input_string}")
+# print(f"Output String: {reverse_str}")
+
+
+# check whether a string is palindrome or not
+
+# input_string = input("Enter String: ")
+# is_palindrome_str = False
+
+# output_str = ''
+
+# for char in range(len(input_string)-1,-1,-1):
+#     output_str += input_string[char]
+
+
+# if input_string.upper() == output_str.upper():
+#     is_palindrome_str = True
+
+
+# result = f"{input_string} is a palindrome string." if is_palindrome_str else f"{input_string} is not a palindrome string."
+
+# print(result)
+
+# Program to Count all letters, digits, and special symbols from a given
+# string
+# Given: str1 = "P@#yn26at^&i5ve"
+# Expected Outcome:
+# Total counts of chars, digits, and symbols
+# Chars = 8
+# Digits = 3
+# Symbol = 4
+
+# input_str = input("Enter String: ")
+# count_digit = 0
+# count_symbol = 0
+# count_letters = 0
+
+# for char in input_str:
+#     if char.isdigit():
+#         count_digit += 1
+
+#     elif char.isalpha():
+#         count_letters += 1
+
+#     else:
+#         count_symbol += 1
+
+# print(f"Chars = {count_letters}")
+# print(f"Digits = {count_digit}")
+# print(f"Symbols = {count_symbol}")

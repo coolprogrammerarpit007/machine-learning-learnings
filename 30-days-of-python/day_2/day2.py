910d6f1f01aaba240d104950367e4be03407f36c
microprixs <arpit.mishra@microprixs.in>
2025-08-06 13:58:43 +0530

    day-2 challenges are done

---
+++ 30-days-of-python/day_2/day2.py
@@ -0,0 +1,41 @@
+# 30 days of python programming - day2
+# first_name = 'Arpit'
+# last_name = 'Mishra'
+# full_name = first_name + '' + last_name
+# country = 'India'
+# city = 'Jaipur'
+age = 25
+year = 2025
+is_married = False
+is_true = False
+is_light_on = False
+friend1,friend2,friend3,friend4 = 'ARPIT','RAJESH','PRANSHU','PRIYANSHU'
+# print(friend1,friend2,friend3,friend4)
+
+
+# print(type(first_name),type(last_name),type(full_name),type(country),type(city),type(age),type(year),type(is_married),type(is_true),type(is_light_on))
+# first_name_length = len(first_name)
+# result = "First name is long" if len(first_name) > len(last_name) else "Last name is Long String"
+# print(result)
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_two - num_one
+multiply = num_one * num_two
+divide = num_one/num_two
+mod_division = num_one%num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+# print(total,diff,multiply,divide,divmod,mod_division,exp,floor_division)
+
+# # circle_radius = 30
+# circle_radius = float(input("Enter Circle Radius: "))
+# circle_area = 3.14 * circle_radius ** 2
+# circle_circumfrence = 3.14 * 2 * circle_radius
+# print(circle_area,circle_circumfrence)
+
+first_name = input("Enter: ")
+last_name = input("Enter: ")
+age = int(input("Enter: "))
+country = input("Enter: ")
+print(first_name,last_name,age,country)

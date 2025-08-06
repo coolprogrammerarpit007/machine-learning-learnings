492335b872e88921de87095d6bcdc6334b0ec9ca
microprixs <arpit.mishra@microprixs.in>
2025-08-06 12:56:35 +0530

    completed day-1

---
+++ 30-days-of-python/day_1/day1.py
@@ -0,0 +1,48 @@
+# Day 1 Exercise
+num1 = 3
+num2 = 4
+
+print(f"Addition: {num1 + num2}")
+print(f"Subtraction: {num2-num1}")
+print(f"Multiplication: {num1*num2}")
+print(f"Division: {num2/num1}")
+print(f"Exponential: {num1**num2}")
+print(f"Floor Division: {num1//num2}")
+
+
+# *******************************
+
+print(f"Name: Arpit Mishra")
+print("Family:Indian Family")
+print("Country: India")
+print("I am enjoying 30 days of Python")
+
+
+# *******************************
+
+print(f"data-type: {type(10)}")
+print(f"data-type: {type(9.8)}")
+print(f"data-type: {type(3.14)}")
+print(f"data-type: {type(['Asabenah','Python','Finland'])}")
+print(f"data-type: {type('Arpit')}")
+print(f"data-type: {type('Mishra Family')}")
+print(f"data-type: {type('India')}")
+
+
+# *********************************
+
+print("Different data types!")
+print(f"Number: {23}")
+print(f"String: {'Hello World'}")
+print(f"Float: {7.856}")
+print(f"Complex data-type: {5+2j}")
+print(f"Boolean Data Type: {True}")
+print(f"List Data Type: {[1,2,3,4,5,6]}")
+print(f"Tuple Data Type: {(1,2,3,4,5,6)}")
+print({
+    "name":"Arpit",
+    "age":25,
+    "isMarried":False,
+    "profession":"Engineer"
+})
+print({78,25,95,67,85})
